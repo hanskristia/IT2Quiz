ad2b570fcc8d19112a8b61221d5bc1361e80bfe0
hkvie <54811308+hkvie@users.noreply.github.com>
2024-11-04 13:35:26 +0100

    Oppdateringer, liten omskriving
    Bedre lesbarhet, har trukket ut koden for å sjekke om quizen er over, og lagt inn noen flere type-hints

--- quiz.py
+++ quiz.py
@@ -1,7 +1,8 @@
-import random
+from random import randint,choice, shuffle
 class SpørsmålQuiz:
     """klasse for hvert quizspørsmål. Inneholder et riktig svar og andre svaralternativer
     Har en metode for å gi svar og svaralternativer i en tilfeldig rekkefølge, med fasit: stillSpørsmål()"""
+    
     def __init__(self, spørsmål:str, riktig:str, svaralternativer:list ) -> None:
         self.spørsmål=spørsmål
         self.riktig=riktig
@@ -9,85 +10,94 @@ class SpørsmålQuiz:
     
     def giSpørsmål(self)-> tuple[str,str, list]:
         """ returnerer spørsmålet, riktig svar og listen med svaralternativer"""
-        
         return self.spørsmål, self.riktig, self.svaralternativer
-# En endring som er viktig
+
 
 class Quiz:
     
-    
     def __init__(self) -> None:
-        self.stilteSpørsmål=[] #indekser over spørsmål som er stilt
+        self.stilteSpørsmål=[]
         self.spørsmål=[] 
+        self.poeng = 0
+        self.poengGrense = 2
+
     def setSpørsmål(self,spm:SpørsmålQuiz):
         """ Legger til et nytt spørsmål til denne quizen"""
         self.spørsmål.append(spm)
     
-    def getNyttspørsmål(self):
+    def getNyttSpørsmål(self):
         """ Henter et spørsmål som ikke er stilt iløpet av denne quizen. """
-        indeks= random.randint(0,len(self.spørsmål)-1)
-        while indeks in self.stilteSpørsmål:
-            indeks= random.randint(0,len(self.spørsmål)-1)
-            #TODO fiks
-        self.stilteSpørsmål.append(indeks)
-        return self.spørsmål[indeks]
+        nytt_spørsmål = choice(self.spørsmål)
+        self.spørsmål.remove(nytt_spørsmål) #flytter spørsmålet
+        self.stilteSpørsmål.append(nytt_spørsmål)
+        return nytt_spørsmål
 
 
-    def stillSpørsmål(self, spørsmål) -> int:
+    def stillSpørsmål(self, spørsmål:SpørsmålQuiz) -> int:
             """ printer en liste med spørsmålet og returnerer indeksen til riktig svar"""
-            spørsmålstekst,riktig, alternativer=spørsmål.giSpørsmål()
+            spørsmålstekst, riktig, alternativer=spørsmål.giSpørsmål()
 
-            fasit=random.randint(0,len(alternativer))
             utskrift=self.stokk(alternativer)
+            fasit=randint(0,len(utskrift))
             utskrift.insert(fasit, riktig)
 
-            print (spørsmålstekst)
+            print(spørsmålstekst)
             for i in range(len(utskrift)):
                 print(f"{i+1}. ", utskrift[i])
 
             return fasit
 
-    def stokk(self, liste):
+    def stokk(self, liste:list) -> list:
         """ stokker en liste"""
         stokket=[]
-        for i in range(len(liste)):
-            ny=liste.pop()
-            stokket.insert(random.randint(0,len(stokket)),ny)
+        while liste:
+            valgt=choice(liste)
+            liste.remove(valgt)
+            stokket.append(valgt)
         return stokket
+
+    def erFerdig(self)->bool:
+        if self.poeng >= self.poengGrense:
+            return True
+        elif len(self.spørsmål) == 0:
+            return  True
+        else:
+            return False # Vi er ikke ferdige
 
     def play(self):
         """ en fun-fun-funksjon som lar deg spille et quiz spill.
         Målet er å komme til 2 poeng, raskest mulig.
         """
-        poeng=0
+        
 
-        while poeng<2 or len(self.stilteSpørsmål)== len(self.spørsmål):
+        while not self.erFerdig():
 
-            spm = self.getNyttspørsmål() # Henter et nytt spørsmål
+            spm = self.getNyttSpørsmål() # Henter et nytt spørsmål
             indeksFasit=self.stillSpørsmål(spm)  # Stiller spørsmålet med tilfeldig rekkefølge
             svar=int(input("Hva er riktig svaralternativ?")) #tar inn et svaralternativ. Disse går fra 1 og oppover.
 
             if indeksFasit==svar-1:
-                poeng+=1
+                self.poeng+=1
                 print("riktig!")
             else:
                 print("feil, riktig svar er ", spm.riktig)
 
-        if poeng >= 2 :
-            print("hurra! du fikk", poeng," på ", len(self.stilteSpørsmål), "forsøk")
+        if self.poeng >= self.poengGrense :
+            print("hurra! du fikk", self.poeng," på ", len(self.stilteSpørsmål), "forsøk")
         else:
             print("oi, vi gikk vist tomt for spørsmål...")
+            print(self.spørsmål)
 
 
 matteQuiz = Quiz()
 spm1=SpørsmålQuiz("hva er 2+2?" , "4" , [1,2,3])
 spm2=SpørsmålQuiz("hva er 3+2?" , "5" , [1,2,3])
 spm3=SpørsmålQuiz("hva er 3+3?" , "6" , [1,2,3])
-spm4=SpørsmålQuiz("hva er 3+4?" , "7" , [1,2,3])
-spm5=SpørsmålQuiz("hva er 3+5?" , "8" , [1,2,3])
-spm5=SpørsmålQuiz("hva er 3+6?" , "9" , [1,2,3])
 matteQuiz.setSpørsmål(spm1)
 matteQuiz.setSpørsmål(spm2)
 matteQuiz.setSpørsmål(spm3)
-print("Hei, fra HAns")
+matteQuiz.setSpørsmål(SpørsmålQuiz("hva er 3+4?" , "7" , [1,2,3]))
+matteQuiz.setSpørsmål(SpørsmålQuiz("hva er 3+5?" , "8" , [1,2,3]))
+matteQuiz.setSpørsmål(SpørsmålQuiz("hva er 3+6?" , "9" , [1,2,3]))
+
 matteQuiz.play()
