608698d0d69240311e7a8e91309b317277a24e45
hkvie <54811308+hkvie@users.noreply.github.com>
2024-11-04 14:03:28 +0100

    Omorganisering
    Har flyttet ansvar fra Quiz til spørsmål.

--- quiz.py
+++ quiz.py
@@ -1,17 +1,33 @@
 from random import randint,choice, shuffle
 class SpørsmålQuiz:
     """klasse for hvert quizspørsmål. Inneholder et riktig svar og andre svaralternativer
-    Har en metode for å gi svar og svaralternativer i en tilfeldig rekkefølge, med fasit: stillSpørsmål()"""
+    Har en metode for å gi svar og svaralternativer i en tilfeldig rekkefølge, med fasit: giSpørsmål()"""
     
-    def __init__(self, spørsmål:str, riktig:str, svaralternativer:list ) -> None:
+    def __init__(self, spørsmål:str, fasit:str, svaralternativer:list ) -> None:
         self.spørsmål=spørsmål
-        self.riktig=riktig
-        self.svaralternativer=svaralternativer
-    
-    def giSpørsmål(self)-> tuple[str,str, list]:
-        """ returnerer spørsmålet, riktig svar og listen med svaralternativer"""
-        return self.spørsmål, self.riktig, self.svaralternativer
+        self.fasit_indeks = randint(0,len(svaralternativer))
+        
+        self.svaralternativer=self.stokkListe(svaralternativer)
+        self.svaralternativer.insert(self.fasit_indeks,fasit)
 
+    def giSpørsmål(self)-> tuple[str,list]:
+        """ returnerer spørsmålet, listen med svaralternativer"""
+        return self.spørsmål, self.svaralternativer
+
+    def erRiktig(self,svar:int):
+        if svar-1 == self.fasit_indeks:
+            return True
+        else: 
+            return False
+    
+    def stokkListe(self, liste:list)->list:
+        """ stokker en liste"""
+        stokket=[]
+        while liste:
+            valgt=choice(liste)
+            liste.remove(valgt)
+            stokket.append(valgt)
+        return stokket
 
 class Quiz:
     
@@ -25,7 +41,7 @@ class Quiz:
         """ Legger til et nytt spørsmål til denne quizen"""
         self.spørsmål.append(spm)
     
-    def getNyttSpørsmål(self):
+    def hentSpørsmål(self)->SpørsmålQuiz:
         """ Henter et spørsmål som ikke er stilt iløpet av denne quizen. """
         nytt_spørsmål = choice(self.spørsmål)
         self.spørsmål.remove(nytt_spørsmål) #flytter spørsmålet
@@ -35,26 +51,12 @@ class Quiz:
 
     def stillSpørsmål(self, spørsmål:SpørsmålQuiz) -> int:
             """ printer en liste med spørsmålet og returnerer indeksen til riktig svar"""
-            spørsmålstekst, riktig, alternativer=spørsmål.giSpørsmål()
-
-            utskrift=self.stokk(alternativer)
-            fasit=randint(0,len(utskrift))
-            utskrift.insert(fasit, riktig)
+            # spørsmålstekst, riktig, alternativer=spørsmål.giSpørsmål()
+            spørsmålstekst, alternativer = spørsmål.giSpørsmål()
 
             print(spørsmålstekst)
-            for i in range(len(utskrift)):
-                print(f"{i+1}. ", utskrift[i])
-
-            return fasit
-
-    def stokk(self, liste:list) -> list:
-        """ stokker en liste"""
-        stokket=[]
-        while liste:
-            valgt=choice(liste)
-            liste.remove(valgt)
-            stokket.append(valgt)
-        return stokket
+            for i in range(len(alternativer)):
+                print(f"{i+1}. ", alternativer[i])
 
     def erFerdig(self)->bool:
         if self.poeng >= self.poengGrense:
@@ -68,15 +70,12 @@ class Quiz:
         """ en fun-fun-funksjon som lar deg spille et quiz spill.
         Målet er å komme til 2 poeng, raskest mulig.
         """
-        
-
         while not self.erFerdig():
 
-            spm = self.getNyttSpørsmål() # Henter et nytt spørsmål
-            indeksFasit=self.stillSpørsmål(spm)  # Stiller spørsmålet med tilfeldig rekkefølge
+            spm = self.hentSpørsmål() # Henter et nytt spørsmål
             svar=int(input("Hva er riktig svaralternativ?")) #tar inn et svaralternativ. Disse går fra 1 og oppover.
 
-            if indeksFasit==svar-1:
+            if spm.erRiktig(svar):
                 self.poeng+=1
                 print("riktig!")
             else:
